b939b453eb71d09bdec109a277d449f86b02d842
Sageer Ahmed <160208153+sageerify@users.noreply.github.com>
2024-12-19 05:03:38 -0800

    Frontend

---
+++ Functions.js
@@ -0,0 +1,21 @@
+function nice(name){
+    console.log("Hey" +name+"you are nice")
+    console.log("Hey" +name+"you are nice")
+    console.log("Hey" +name+"you are nice")
+    console.log("Hey" +name+"you are nice")
+}
+nice("Rohan ")
+nice("Shivam")
+
+function sum(a,b){
+    return a+b
+}
+result = sum(5, 5)
+result = sum(5, 8)
+result = sum(5, 0)
+result = sum(5, 5)
+// sum(5+5)
+console.log("The sum of these numbers is: ", result)
+console.log("The sum of these numbers is: ", result)
+console.log("The sum of these numbers is: ", result)
+console.log("The sum of these numbers is: ", result)
---
+++ variable.js
@@ -0,0 +1,40 @@
+console.log('Hey this is console of javaScript')
+let a=5;
+let b=6;
+let c="Harry";
+// let _a="Shuban"let
+// var 4=10; Not correct and not allowed
+//let is blogs code. always used let.
+
+console.log(a+b+c)
+console.log(typeof a, typeof c, typeof b)
+// const a1=6; also not allowed
+// a1= a1 + 1; this is not allowed because a1 is constant
+//Two types of datatype
+//1. Premited Data Type and Object data type.
+let x="Harry Bhati";
+let y="22";
+let z= 4.44
+;
+const p=true;
+let q = undefine;
+let r = null;
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+// type of null is object
+// object is key value variable
+
+
+
+
+
+let o= {
+    name: "Harry",
+    "job code": "5600";
+
+}
+console.log(o)
+o.salary ="100crored";
+console.log(o);
+o.salary ="Is handsome";
+
